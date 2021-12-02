b5405ecc9a455fd7d2032b387f00e3209c62c41f
class02 <class02@192.168.0.57>
2021-12-02 17:31:17 +0900

    커밋

--- src/main/java/com/api/test/removeCotroller.java
+++ src/main/java/com/api/test/removeCotroller.java
@@ -1,18 +1,16 @@
 package com.api.test;
 
-import java.util.HashMap;
-import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.servlet.ModelAndView;
 
 import api.dao.MemDAO;
-import api.u_member.vo.UmemVO;
+
 
 @Controller
 public class removeCotroller {
@@ -34,19 +32,5 @@ public class removeCotroller {
 		
 		return mv;
 	}
-	
-	@RequestMapping("/mypage/removess")
-	@ResponseBody
-	public Map<String, String> removess(String id){
-		Map<String, String> map = new HashMap<String, String>();
-		
-		int cnt = m_dao.del(id);
-		System.out.println(id);
-		if(cnt > 0)
-			map.put("result", "1");
-		else
-			map.put("result", "2");
-		
-		return map;
-	}
+
 }
