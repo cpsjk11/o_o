f35d59eca54c6a11e3b6fe42ac0beda705fef8aa
class02 <class02@192.168.0.57>
2021-12-10 14:06:27 +0900

    serch

--- src/main/java/com/api/test/searchController.java
+++ src/main/java/com/api/test/searchController.java
@@ -6,50 +6,55 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import api.action.BbsPaging;
+import api.action.mypagePaging;
 import api.dao.BbsDAO;
+import api.dao.MemDAO;
+import api.u_member.vo.TrVO;
 import api.vo.BbsVO;
 
 @Controller
 public class searchController {
 	
 	@Autowired
+	private MemDAO m_dao;
+	
+	@Autowired
 	private BbsDAO b_dao;
 	
-	int block_list = 5;
-	int block_page = 3;
+	public final int BLOCK_LIST = 5;
+	
+	public final int BLOCK_PAGE = 5;
 	int nowPage;
 	int rowTotal;
 	String pageCode;
 
 	@RequestMapping("/mypage/search")
-	public ModelAndView inter(String cPage, String bname) {
+	public ModelAndView inter(String cPage) {
 		ModelAndView mv = new ModelAndView();
 		
 		if (cPage == null)
 			nowPage = 1;
 		else
 			nowPage = Integer.parseInt(cPage);
-
-		if (bname == null)
-			bname = "일반회원";
 		
-		rowTotal = b_dao.getTotalCount(bname, "0");
-		BbsPaging page = new BbsPaging(nowPage, rowTotal, block_list, block_page, bname);
+	
+		rowTotal = m_dao.getTotalCount();
+		mypagePaging page = new mypagePaging(nowPage, rowTotal, BLOCK_LIST, BLOCK_PAGE);
 		
 		int begin = page.getBegin();
 		int end = page.getEnd();
 		
 		pageCode = page.getSb().toString();
 		
-		BbsVO[] ar = b_dao.getList(begin, end, bname, "0");
+		TrVO[] ar = m_dao.getList(begin, end);
 		
-		mv.addObject("bname",bname);
 		mv.addObject("ar", ar);
 		mv.addObject("nowPage", nowPage);
 		mv.addObject("rowTotal", rowTotal);
-		mv.addObject("blockList", block_list);
+		mv.addObject("blockList", BLOCK_LIST);
 		mv.addObject("pageCode", pageCode);
-		mv.setViewName("최근조회한훈련");
+		
+		mv.setViewName("/최근조회한훈련");
 		
 		return mv;
 	}
