0b623fbf58ecb5dc94c491ef82301e6b79bb9cbb
82105 <82105@DESKTOP-7RKN2CM>
2021-12-16 16:35:59 +0900

    추천 훈련과정 view연결

--- src/main/java/com/api/test/Api.java
+++ src/main/java/com/api/test/Api.java
@@ -34,7 +34,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.JsonParser;
 
 import api.action.RecommendedSchool;
