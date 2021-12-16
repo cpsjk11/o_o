5dc0e993c917321a9295a8fbd8f34709ce58651f
user <user@DESKTOP-JF8RFK5>
2021-12-16 12:46:40 +0900

    edit

--- src/main/java/api/dao/TraDAO.java
+++ src/main/java/api/dao/TraDAO.java
@@ -17,7 +17,7 @@ public class TraDAO {
 	private SqlSessionTemplate ss;
 	
 	// 훈련 등록 기능
-	public int add(String tr_id, String tr_name, String tr_degr, String price, String person, String start, String manager, String imageCode, String addr, String u_id) {
+	public int add(String tr_id, String tr_name, String tr_degr, String price, String person, String start, String manager, String imageCode, String addr, String tr_u_id) {
 		Map<String, Object> map = new HashMap<String, Object>();
 		
 		String price1 = price.replace(",", ""); 
@@ -32,7 +32,7 @@ public class TraDAO {
 		map.put("stat", 1);
 		map.put("imageCode", imageCode);
 		map.put("addr", addr);
-		map.put("u_id", u_id);
+		map.put("tr_u_id", tr_u_id);
 		
 		return ss.insert("tra.add", map);
 	}
